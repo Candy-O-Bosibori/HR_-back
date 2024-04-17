ce7dc1768a05347fd40427966af46c31ee8d8351
Hamdi Adan <hhhaji013@gmail.com>
2024-04-17 12:14:43 +0300

    created review model

--- app/models.py
+++ app/models.py
@@ -6,3 +6,9 @@ from sqlalchemy.ext.associationproxy import association_proxy
 db = SQLAlchemy()
 
 # Define Models
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews"
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String, nullable=False)
