d15593b1cb63035986426597288606b1fbc9f848
Alex Kimeu <alexkimeu1999@gmail.com>
2024-04-17 10:55:23 +0300

    Initialized repo

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates, relationship
+from sqlalchemy.ext.associationproxy import association_proxy
+
+db = SQLAlchemy()
+
+# Define Models
