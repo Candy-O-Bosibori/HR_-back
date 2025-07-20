5535f9f36c8ee2b339692d72a988a90bc5da77f3
Candy Ongaro <candybosibori04@gmail.com>
2025-07-20 14:26:21 -0400

    done

--- app.py
+++ app.py
@@ -299,4 +299,5 @@ class LeaveByEmployeeID(Resource):
 api.add_resource(LeaveByEmployeeID, '/leaves/employee')
     
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
